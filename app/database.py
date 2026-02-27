8fb281157be209d5b396673ea0cf53a574e647de
Saraththecoder <sarathkumar86518@gmail.com>
2026-02-28 01:54:13 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import settings
 
-client = AsyncIOMotorClient(settings.MONGO_URI)
+client = AsyncIOMotorClient(
+    settings.MONGO_URI,
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
+
 db = client["sakhi_db"]
 
 users_collection = db["users"]
